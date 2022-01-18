3754b109d3f56678f927d7cf59927783c3a7a42f
cesar cobos <cesarcoboscv@gmail.com>
2022-01-18 10:10:03 -0600

    Improving transitions and icons

--- js/script.js
+++ js/script.js
@@ -1,25 +1,44 @@
 // Definiendo una funciona para activar el modo oscuro
 function toggle_dark_mode(){ //este nombre también va ligado al botón con el evento onClick
-    // identificando el id "dark" en el HTML y asignandolo a la variable "element"
+// identificando el id "dark" en el HTML y asignandolo a la variable "element"
     var element = document.getElementById("dark")
-    //Aquí se hace el cambio de la clase por "dark"
+//Aquí se hace el cambio de la clase por "dark"
     element.classList.toggle("dark")
-    //solo un mensaje para la consola
+//solo un mensaje para la consola
     console.log("Dark Mode Activaded")
+//reemplazando la clase para el boton de Dark Mode
+//Cambia el icono segun el estado en el que se encuentra
+    var buttonIcon = document.getElementById("buttonIcon"); 
+    if(buttonIcon.className === "fas fa-toggle-on"){
+        buttonIcon.className = "fas fa-toggle-off";
+    }else{
+        buttonIcon.className = "fas fa-toggle-on";
+    }
+//cambiando iconos segun el estado del Dark mode
+//Esto puede servir para cambiar estilos de iconos en la barra de redes
+    var social = document.getElementById("social"); 
+    if(buttonIcon.className === "fas fa-toggle-on"){
+        social.className = "fab fa-twitter";
+    }else{
+        social.className = "fab fa-facebook";
+    }
+
+    
 }
+    
 
 
 //Navbar onscrol change color
 //Vinculando Js con el elemento en HTML
 var barNav = document.getElementById("nav");
 window.onscroll = function(){ //Definiendo funcion
-    //condicion, si la pantalla hace scroll por 180 entonces
-    //Se agrega la clase "scrolled" al id "nav"
+//condicion, si la pantalla hace scroll por 180 entonces
+//Se agrega la clase "scrolled" al id "nav"
     if (document.documentElement.scrollTop >= 180){
         barNav.classList.add("scrolled"); 
         console.log("Scroll On - color change");
     }else{
-        //pero si es menor a 180, entonces se va a eliminar la clase "scrolled" del id "nav"
+//pero si es menor a 180, entonces se va a eliminar la clase "scrolled" del id "nav"
         barNav.classList.remove("scrolled")
         console.log("Scroll Off - color change");
     }
